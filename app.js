e79271dde0c445d53aee9c9bd832ed84b885bfa3
Bill Brookfield <billyb@tampabay.rr.com>
2015-10-11 13:53:38 -0400

    'stuff'

--- app.js
+++ app.js
@@ -10,23 +10,39 @@ var alexa = require('alexa-app');
 
 
 var alexaApp = new alexa.app('sample');
-
+var temperature = 60;
 alexaApp.launch(function(req, res) {
     console.log('REQUEST', JSON.stringify(req));
     res.say('I am amazing');
 });
 
-alexaApp.intent('sayNumber',
+alexaApp.intent('setTemp',
     {
-        "slots":{"number":"NUMBER"}
-        ,"utterances":[ "say the number {number}" ]
+        "slots": {
+            "temperature": "NUMBER"
+        }
+        ,"utterances":[ "set the temperature to {temperature}" ]
     },
     function(request, response) {
         console.log(JSON.stringify(request));
-        var number = request.slot('number');
-        response.say("You asked for the number " + number);
+        temperature = request.slot('temperature');
+        response.say("I have set the temperature to " + temperature);
     }
 );
+
+alexaApp.intent('getTemp',
+    {
+        "slots": {
+            "temperature": "NUMBER"
+        }
+        ,"utterances":["what is the temperature"]
+    },
+    function(request, response) {
+        console.log(JSON.stringify(request));
+        response.say("the temperature is " + temperature);
+    }
+);
+
 
 // Manually hook the handler function into express
 app.post('/sample',function(req,res) {
