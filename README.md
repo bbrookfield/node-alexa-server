e073f7937a21fad8e445a26f76abb35d7985d754
Bill Brookfield <billyb@tampabay.rr.com>
2015-10-14 08:42:17 -0400

    ver 1.0

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # alexa-thermostat
 
-Simple Alexa skills app to request the current temperature and target temperature on
-a filtret CT50 thermostat. I have this hosted on heroku due to the SSL requirements of
-amazon.
+Simple Alexa skills app to request and set the current temperature and target temperature on
+a filtret CT50 thermostat.
 
 The request is sent to my thermostat via a Dynamic DNS and my router port forwards to
 my internal thermostat's IP address.
 
-This code is a mis mash of other code I found while researching building an Alexa Skill
+This code is a mis mash of other code I found while researching building an Alexa Skill.
+
+Big thanks to Matt Kruse for alexa-app. alexa-app makes the process of writing alexa skills simple.
 
 Bill B
--- app.js
+++ app.js
@@ -1,48 +1,47 @@
+// Requires for this skill
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var request = require('request');
+var alexa = require('alexa-app');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Manually hook the handler function into express
-
-var alexa = require('alexa-app');
-
+// Create new app
 var alexaApp = new alexa.app('thermostat');
+
+// process launch request when no utterances detected
 alexaApp.launch(function(req, res) {
     console.log('REQUEST', JSON.stringify(req));
     res.say("You can say, Alexa ask thermostat what is the temperature, or Alexa tell thermostat set the temperature to 75!");
 });
 
+// process set temperature request
 alexaApp.intent('setTemp',
     function(req, res) {
         console.log(JSON.stringify(req));
         request.post(process.env.THERMOSTAT_URL + '/tstat', {json: {t_cool: parseFloat(req.slot('temperature'))}});
-        res.say("I have set the thermostat temperature to " + parseInt(req.slot('temperature')));
-    }
-);
+        res.say("Thermostat is set to " + parseInt(req.slot('temperature')) + " degrees");
+});
 
+// process get temperature request
 alexaApp.intent('getTemp',
     function(req, res) {
         console.log(JSON.stringify(req));
         request(process.env.THERMOSTAT_URL + '/tstat', function (error, response, body) {
             body = JSON.parse(body);
-            res.say("The current temperature for your thermostat is " + body.temp + " degrees, and the target temperature is " + body.t_cool + " degrees.");
+            res.say("Thermostat current temperature is " + body.temp + " degrees, the target temperature is " + body.t_cool + " degrees.");
             res.send();
         });
         return false;
-    }
-);
-
-
+});
 
 // Manually hook the handler function into express
 app.post('/thermostat',function(req, res) {
     alexaApp.request(req.body)        // connect express to alexa-app
-        .then(function(response) { // alexa-app returns a promise with the response
-            res.json(response);      // stream it to express' output
+        .then(function(response) {    // alexa-app returns a promise with the response
+            res.json(response);       // stream it to express' output
         });
 });
 
