819d7b4e93025e744ce754fdf18c2f64c9a11e20
dvideby0 <rbrookfield@352inc.com>
2015-10-25 21:33:31 -0400

    updating app to handle both direct TV and thermostat

--- app_modules/directTV/index.js
+++ app_modules/directTV/index.js
@@ -3,11 +3,14 @@ var request = require('request');
 var alexa = require('alexa-app');
 
 module.exports = function() {
-  global.tv = {
-
-  }
+  global.tv = directTVApp
 };
 
-function changeChannel(channel) {
-  //Your code to change the channel
-}
+var directTVApp = new alexa.app('directTV');
+
+// process set temperature request
+directTVApp.intent('setChannel', function(req, res) {
+  request.get(process.env.DVR_URL + '/tv/tune?major=' + parseInt(req.slot('Channel')));
+  res.card("Direct TV Skill","TV set to channel " + parseInt(req.slot('Channel')));
+  res.say("I have set the TV to channel " + parseInt(req.slot('Channel')) + " as requested");
+});
