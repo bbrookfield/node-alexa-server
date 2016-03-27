8b5dca7cfe68061c2c0d864b839042cd12bcbd46
Bill Brookfield <billyb@tampabay.rr.com>
2016-03-27 17:45:19 -0400

    fixed int issue

--- app_modules/alexa-Yamaha-Receiver/index.js
+++ app_modules/alexa-Yamaha-Receiver/index.js
@@ -4,20 +4,26 @@ var alexa = require('alexa-app');
 
 var yamahaApp = new alexa.app('yamaha');
 
-yamahaApp.intent('setInput', function(req, res) {
-  request.post(process.env.YAMAHA_URL + '/?'+req.slot('Input'));
-  res.card("Yamaha Receiver Skill","Yamaha Receiver set to input " + req.slot('Input'));
-  res.say("Yamaha Receiver set to input " + req.slot('Input'));
+yamahaApp.intent('setInput', function (req, res) {
+    request.post(process.env.YAMAHA_URL + '/?' + req.slot('Input'), function (err, response, body) {
+        if (err) {
+            res.say(err);
+        } else {
+            res.card("Yamaha Receiver Skill", "Yamaha Receiver set to input " + req.slot('Input'));
+            res.say("Yamaha Receiver set to input " + req.slot('Input'));
+        }
+    });
+    return false;
 });
 
 
 // process launch request when no utterances detected
-yamahaApp.launch(function(req, res) {
-  console.log('REQUEST', JSON.stringify(req));
-  res.say("You can say, Set input to TV, or set input to echo!");
+yamahaApp.launch(function (req, res) {
+    console.log('REQUEST', JSON.stringify(req));
+    res.say("You can say, Set input to TV, or set input to echo!");
 });
 
-module.exports = function(cb) {
-  global.yamaha = yamahaApp;
-  cb();
+module.exports = function (cb) {
+    global.yamaha = yamahaApp;
+    cb();
 };
